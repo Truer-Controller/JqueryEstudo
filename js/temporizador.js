d6c234884ff69e72dc774ab617647336ba032ec0
Rafael Augusto <rafajunior2907@gmail.com>
2024-01-12 19:30:13 -0300

    começando o temporizador

---
+++ js/temporizador.js
@@ -0,0 +1,12 @@
+(function ($) {
+    $.fn.temporizador = function(opcoes){
+        const opcoesFinais = $.extends({
+            mensagem: 'Em breve!',
+            horario: '23:59:59'
+        }, opcoes)
+
+        const horaDezena =  $()
+
+        return this
+    }
+})(jQuery)
